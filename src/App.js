eaf92c0d9f50e51c09cd1693b919ceb35bc4318b
robertomalheiros <robertomalheirosb@gmail.com>
2022-11-09 20:41:49 -0300

    Iniciando Lab

--- src/App.js
+++ src/App.js
@@ -1,23 +1,37 @@
-import logo from './logo.svg';
-import './App.css';
+import AddFoodForm from './components/AddFoodForm';
+import FoodBox from './components/FoodBox';
+import Search from './components/Search';
+import AllFoods from './foods.json';
+import { Row, Divider, Button } from 'antd';
+import { useState } from 'react';
 
 function App() {
+  const [foods, setFoods] = useState(AllFoods);
+  console.log(foods);
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <AddFoodForm />
+
+      <Button> Hide Form / Add New Food </Button>
+
+      <Search />
+
+      <Divider>Food List</Divider>
+
+      <Row style={{ width: '100%', justifyContent: 'center' }}>
+        {foods.map((element) => {
+          return (
+            <FoodBox
+              image={element.image}
+              name={element.name}
+              calories={element.calories}
+              servings={element.servings}
+              key={element.name}
+            />
+          );
+        })}{' '}
+        />
+      </Row>
     </div>
   );
 }
--- src/components/AddFoodForm.js
+++ src/components/AddFoodForm.js
@@ -1,3 +1,7 @@
+// Style Guide:
+// In this file you can find a reference example of the structure
+// and content that the component should render.
+// Remember to import Ant Design components before using them.
 import { Divider, Input } from 'antd';
 
 // Iteration 4
